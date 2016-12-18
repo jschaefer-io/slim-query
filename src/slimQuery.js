07d37e883b993731a0102d4b2a3a1044325b2f05
Jannik Schäfer <me@jschaefer.io>
2016-12-18 23:33:59 +0100

    Base Bootstrao to expand upon

---
+++ src/slimQuery.js
@@ -0,0 +1,64 @@
+
+/**
+ * slimQuery.js
+ * ---------------
+ */
+(function(){
+
+	function slimQuery(query, extend){
+		if (extend) {
+			this.extend = function(obj){
+				for( var prop in obj){
+					if (!(prop in this)) {
+						slimQuery.prototype[prop] = obj[prop];
+					}
+				}
+			};
+			return this;
+		};
+		this.on = function(name, handler){
+			var test = true;
+			this.each(function(item){
+				item.addEventListener(name, handler);
+			});
+			
+		};
+		this.each = function(handler){
+			for(var item in this.data){
+				if (parseInt(item)||item === '0') {
+					handler(this.data[item]);
+				}
+			}
+		};
+
+		if (query) {
+			var type = typeof query;
+			if (type == 'string') {
+				this.data = document.querySelectorAll(query);
+			}
+			if (type == 'object') {
+				if( Array.isArray(query) ){
+					this.data = query;
+				}
+				else{
+					this.data = [query];
+				}
+			}		
+		}
+	}
+
+	// Returns a new sq Instance
+	function sqInstance(query, extend){
+		return new slimQuery(query, extend);
+	}	
+
+	// Main entry point
+	window.sq = function(query){
+		// for global slimQuery actions use the global "slimquery" variable');
+		return sqInstance(query, false);
+	}
+
+	// Extend entry point
+	window.slimquery = sqInstance(document, true);
+
+})();
